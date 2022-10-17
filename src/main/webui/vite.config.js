04e0d339fc5d4a9b917f5e1d6f17c57b5fb745dd
Johannes Fauske <johannfa@gmail.com>
2022-10-17 16:16:48 +0200

    react version

--- src/main/webui/vite.config.js
+++ src/main/webui/vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [react()],
   server: {
-    //strictPort: true,
     hmr: {
       port: 5173
     }
   }
-
 })
